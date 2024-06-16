70acb6194d5365d0aecf015e0a1caf35bc147039
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2024-06-15 23:40:47 -0300

    :sparkles: Conexão com Firebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,166 +1,134 @@
-/**
- * SYNTAX:
- *
- * RealtimeDatabase::get(<AsyncClient>, <path>, <AsyncResultCallback>, <SSE>, <uid>);
- *
- * RealtimeDatabase::get(<AsyncClient>, <path>, <DatabaseOption>, <AsyncResultCallback>, <uid>);
- *
- * <AsyncClient> - The async client.
- * <path> - The node path to get/watch the value.
- * <DatabaseOption> - The database options (DatabaseOptions).
- * <AsyncResultCallback> - The async result callback (AsyncResultCallback).
- * <uid> - The user specified UID of async result (optional).
- * <SSE> - The Server-sent events (HTTP Streaming) mode.
- *
- * The complete usage guidelines, please visit https://github.com/mobizt/FirebaseClient
- */
-
-#include <Arduino.h>
-#if defined(ESP32) || defined(ARDUINO_RASPBERRY_PI_PICO_W) || defined(ARDUINO_GIGA)
 #include <WiFi.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#elif __has_include(<WiFiNINA.h>) || defined(ARDUINO_NANO_RP2040_CONNECT)
-#include <WiFiNINA.h>
-#elif __has_include(<WiFi101.h>)
-#include <WiFi101.h>
-#elif __has_include(<WiFiS3.h>) || defined(ARDUINO_UNOWIFIR4)
-#include <WiFiS3.h>
-#elif __has_include(<WiFiC3.h>) || defined(ARDUINO_PORTENTA_C33)
-#include <WiFiC3.h>
-#elif __has_include(<WiFi.h>)
-#include <WiFi.h>
-#endif
+#include <Firebase_ESP_Client.h>
+#include <secrets.h>
 
-#include <FirebaseClient.h>
 
-#define WIFI_SSID "WIFI_AP"
-#define WIFI_PASSWORD "WIFI_PASSWORD"
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
 
-// The API key can be obtained from Firebase console > Project Overview > Project settings.
-#define API_KEY "Web_API_KEY"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
 
-// User Email and password that already registerd or added in your project.
-#define USER_EMAIL "USER_EMAIL"
-#define USER_PASSWORD "USER_PASSWORD"
-#define DATABASE_URL "URL"
+// Defines the Digital Pin of the "On Board LED".
+#define On_Board_LED 2
 
-void asyncCB(AsyncResult &aResult);
 
-void printResult(AsyncResult &aResult);
+//Define Firebase Data object.
+FirebaseData fbdo;
 
-DefaultNetwork network;
+FirebaseAuth auth;
+FirebaseConfig config;
 
-UserAuth user_auth(API_KEY, USER_EMAIL, USER_PASSWORD);
+//======================================== Millis variable to read data from firebase database.
+unsigned long sendDataPrevMillis = 0;
+const long intervalMillis = 10000; //--> Read data from firebase database every 10 seconds.
+//======================================== 
 
-FirebaseApp app;
+// Boolean variable for sign in status.
+bool signupOK = false;
 
-#if defined(ESP32) || defined(ESP8266) || defined(ARDUINO_RASPBERRY_PI_PICO_W)
-#include <WiFiClientSecure.h>
-WiFiClientSecure ssl_client;
-#elif defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_UNOWIFIR4) || defined(ARDUINO_GIGA) || defined(ARDUINO_PORTENTA_C33) || defined(ARDUINO_NANO_RP2040_CONNECT)
-#include <WiFiSSLClient.h>
-WiFiSSLClient ssl_client;
-#endif
+float read_random_Float_Val;
+int read_random_Int_Val;
 
-using AsyncClient = AsyncClientClass;
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(115200);
+  Serial.println();
 
-AsyncClient aClient(ssl_client, getNetwork(network));
+  pinMode(On_Board_LED, OUTPUT);
 
-RealtimeDatabase Database;
+  //---------------------------------------- The process of connecting the WiFi on the ESP32 to the WiFi Router/Hotspot.
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("---------------Connection");
+  Serial.print("Connecting to : ");
+  Serial.println(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
 
-bool taskComplete = false;
+    digitalWrite(On_Board_LED, HIGH);
+    delay(250);
+    digitalWrite(On_Board_LED, LOW);
+    delay(250);
+  }
+  digitalWrite(On_Board_LED, LOW);
+  Serial.println();
+  Serial.print("Successfully connected to : ");
+  Serial.println(WIFI_SSID);
+  //Serial.print("IP : ");
+  //Serial.println(WiFi.localIP());
+  Serial.println("---------------");
+  //---------------------------------------- 
 
-void setup()
-{
+  // Assign the api key (required).
+  config.api_key = API_KEY;
 
-    Serial.begin(115200);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  // Assign the RTDB URL (required).
+  config.database_url = DATABASE_URL;
 
-    Serial.print("Connecting to Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print(".");
-        delay(300);
-    }
+  /* Assign the user sign in credentials */
+  auth.user.email = FIREBASE_USER;
+  auth.user.password = FIREBASE_PASSWORD;
+
+  config.token_status_callback = tokenStatusCallback; //--> see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+
+void loop() {
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > intervalMillis || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+
+    //---------------------------------------- Generate random values.
+    int randNumber = random(15, 40);
+    float f = (float)randNumber / 1.01;
+    int i = (int(f*100));
+    float store_random_Float_Val = float(i) / 100;
+    int store_random_Int_Val = random(10, 99);
+    //---------------------------------------- 
+
+    //---------------------------------------- 
     Serial.println();
-    Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
+    Serial.println("---------------Random Value");
+    Serial.print("Random Float_Val : ");
+    Serial.println(store_random_Float_Val);
+    Serial.print("Random Int_Val   : ");
+    Serial.println(store_random_Int_Val);
+    Serial.println("---------------");
+    //---------------------------------------- 
+
+    //---------------------------------------- The process of sending/storing data to the firebase database.
     Serial.println();
-
-    Firebase.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
-
-    Serial.println("Initializing app...");
-
-#if defined(ESP32) || defined(ESP8266) || defined(PICO_RP2040)
-    ssl_client.setInsecure();
-#if defined(ESP8266)
-    ssl_client.setBufferSizes(4096, 1024);
-#endif
-#endif
-
-    initializeApp(aClient, app, getAuth(user_auth), asyncCB, "authTask");
-
-    // Binding the FirebaseApp for authentication handler.
-    // To unbind, use Database.resetApp();
-    app.getApp<RealtimeDatabase>(Database);
-
-    Database.url(DATABASE_URL);
-}
-
-void loop()
-{
-    // The async task handler should run inside the main loop
-    // without blocking delay or bypassing with millis code blocks.
-
-    app.loop();
-
-    Database.loop();
-
-    if (app.ready() && !taskComplete)
-    {
-        taskComplete = true;
-
-        Database.get(aClient, "/test/int", asyncCB, false, "getTask1");
-
-        Database.get(aClient, "/test/string", asyncCB, false, "getTask2");
-
-        // Filtering data
-        // For REST API, indexing the data at /test/filter/json is required when filtering the data, please see examples/Database/Extras/Sync/IndexingData/IndexingData.ino.
-        DatabaseOptions options;
-        options.filter.orderBy("Data").startAt(105).endAt(120).limitToLast(8);
-
-        Database.get(aClient, "/test/filter/json", options, asyncCB, "getTask3");
+    Serial.println("---------------Store Data");
+    digitalWrite(On_Board_LED, HIGH);
+    
+    // Write an Int number on the database path test/random_Float_Val.
+    if (Firebase.RTDB.setFloat(&fbdo, "/air_conditioning/float", store_random_Float_Val)) {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
     }
-}
-
-void asyncCB(AsyncResult &aResult)
-{
-    // WARNING!
-    // Do not put your codes inside the callback and printResult.
-
-    printResult(aResult);
-}
-
-void printResult(AsyncResult &aResult)
-{
-    if (aResult.isEvent())
-    {
-        Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.appEvent().message().c_str(), aResult.appEvent().code());
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    
+    // Write an Float number on the database path test/random_Int_Val.
+    if (Firebase.RTDB.setInt(&fbdo, "/air_conditioning/int", store_random_Int_Val)) {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
     }
 
-    if (aResult.isDebug())
-    {
-        Firebase.printf("Debug task: %s, msg: %s\n", aResult.uid().c_str(), aResult.debug().c_str());
-    }
-
-    if (aResult.isError())
-    {
-        Firebase.printf("Error task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.error().message().c_str(), aResult.error().code());
-    }
-
-    if (aResult.available())
-    {
-        Firebase.printf("task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
-    }
+    digitalWrite(On_Board_LED, LOW);
+    Serial.println("---------------");
+    //---------------------------------------- 
+  }
 }
